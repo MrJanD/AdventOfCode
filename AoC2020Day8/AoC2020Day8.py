4dc99e96df8252e05f5b7dbab309dae9e3aa2957
Jan Deppe <40394719+MrJanD@users.noreply.github.com>
2020-12-09 16:34:21 +0100

    Add files via upload

--- AoC2020Day8/AoC2020Day8.py
+++ AoC2020Day8/AoC2020Day8.py
@@ -1,60 +1,66 @@
-input = [line.strip() for line in open("Inputs/day8", "r")]
+import copy
+
+input = [line.strip() for line in open("../Inputs/day8", "r")]
 
 class Instruction:
         def __init__(self, line):
             self.operation = line.split()[0]
             self.value = int(line.split()[1])
             self.executed = False
-            self.toggle = False
 
         def proceedOperation(self, accumulator, operationIndex):
             if self.operation == "acc":
                 operationIndex += 1
                 accumulator += int(self.value)
-            elif self.operation == "jmp" or (self.operation == "nop" and self.toggle):
+            elif self.operation == "jmp":
                 operationIndex += int(self.value)
-            elif self.operation == "nop" or (self.operation == "jmp" and self.toggle):
+            elif self.operation == "nop":
                 operationIndex += 1
             self.executed = True
             return accumulator, operationIndex
 
         def reset(self):
             self.executed = False
-            self.toggle = False
 
 instructions = []
 for line in input:
     instructions.append(Instruction(line))
 
-def runCode():
+def runCode(instructionSet):
     accumulator = 0
     index = 0
 
-    while index < len(instructions):
-        if not instructions[index].executed:
-            accumulator, index = instructions[index].proceedOperation(accumulator, index)
+    while index < len(instructionSet):
+        if not instructionSet[index].executed:
+            accumulator, index = instructionSet[index].proceedOperation(accumulator, index)
         else:
             break
 
     return accumulator, index
 
-print("Day 8, Part 1: " + str(runCode()[0]))
+print("Day 8, Part 1: " + str(runCode(instructions)[0]))
 
 def resetInstructions():
     for instruction in instructions:
         instruction.reset()
 
-resetInstructions()
-indexToToggle = 0
-inLen = len(input)
-while indexToToggle < len(input):
+def getToggledInstructions(indexToToggle):
+    toggledInstructions = copy.deepcopy(instructions)
+    if "jmp" in toggledInstructions[indexToToggle].operation:
+        toggledInstructions[indexToToggle].operation = "nop"
+    elif "nop" in toggledInstructions[indexToToggle].operation:
+        toggledInstructions[indexToToggle].operation = "jmp"
+    return toggledInstructions
+
+for indexToToggle in range(len(input)):
+    resetInstructions()
     if "acc"in input[indexToToggle]:
         indexToToggle += 1
         continue
-    instructions[indexToToggle].toggle = True
-    accumulator, index = runCode()
+    toggledInstructions = getToggledInstructions(indexToToggle)
+    accumulator, index = runCode(toggledInstructions)
     if index >= len(input):
         print("Day 8, Part 2: " + str(accumulator))
         break
-    resetInstructions()
+
     indexToToggle += 1
