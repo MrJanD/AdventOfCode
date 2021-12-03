5397ed8fb6edcd01ca967d79d5d58315db05d4aa
MrJanD <github@deppe.one>
2021-12-03 23:53:13 +0100

    Solution of day 1 + 2

--- 2021/AoC2021Day01/AoC2021Day01.py
+++ 2021/AoC2021Day01/AoC2021Day01.py
@@ -1,1 +1,24 @@
+input = [int(line.strip()) for line in open("../Inputs/day01", "r")]
 
+def findIncreasingP1():
+    increasingCounter = 0
+    for index, value in enumerate(input):
+        if index > 0 and value > input[index - 1]:
+            increasingCounter += 1
+    return increasingCounter
+
+def findIncreasingWindowsP2():
+    increasingCounter = 0
+    for index, value in enumerate(input):
+        if getThreeSum(index) > getThreeSum(index - 1):
+            increasingCounter += 1
+    return increasingCounter
+
+def getThreeSum(index):
+    sum = 0
+    if index < (len(input) - 2):
+        sum = input[index] + input[index + 1] + input[index + 2]
+    return sum
+
+print("2021 Day 1, Part 1: " + str(findIncreasingP1()))
+print("2021 Day 1, Part 2: " + str(findIncreasingWindowsP2()))
--- 2021/AoC2021Day02/AoC2021Day02.py
+++ 2021/AoC2021Day02/AoC2021Day02.py
@@ -1,1 +1,32 @@
+input = [line.strip() for line in open("../Inputs/day02", "r")]
 
+def getCoordinates(function):
+    x, y, aim = 0, 0, 0
+    for line in input:
+        xStep, yStep, aim = function(line.split()[0], (int)(line.split()[1]), aim)
+        x += xStep
+        y += yStep
+    return x, y, aim
+
+def getIncrement(command, distance, aim):
+    if command == "forward":
+        return (int)(distance), 0, 0
+    if command == "up":
+        return 0, -(int)(distance), 0
+    if command == "down":
+        return 0, (int)(distance), 0
+    return 0, 0, 0
+
+def getCorrectIncrement(command, value, aim):
+    if command == "forward":
+        return (int)(value), (aim * (int)(value)), aim
+    if command == "up":
+        aim -= value
+    if command == "down":
+        aim += value
+    return 0, 0, aim
+
+x, y, aim = getCoordinates(getIncrement)
+print("2021 Day 2, Part 1: " + str(x * y))
+x, y, aim = getCoordinates(getCorrectIncrement)
+print("2021 Day 2, Part 2: " + str(x * y))
