5cb6ddbefee9049ea799fd3f78deb097887a54e4
MrJanD <github@deppe.one>
2021-12-06 13:38:33 +0100

    Cleanup of day 6

--- 2021/AoC2021Day06/AoC2021Day06.py
+++ 2021/AoC2021Day06/AoC2021Day06.py
@@ -1,41 +1,19 @@
-import math
-
 input = [line.strip() for line in open("../Inputs/day06", "r")]
 
-swarm = []
-for fish in input[0].split(','):
-    swarm.append(int(fish))
+def getSwarmAfterDays(days):
+    swarmAgeDistribution = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-def dayCycle(swarm):
-    for index, fish in enumerate(swarm):
-        swarm[index] = fish - 1
-        if swarm[index] == -1:
-            swarm[index] = 6
-            swarm.append(9)
+    for fish in input[0].split(','):
+        swarmAgeDistribution[int(fish)] += 1
 
-for day in range(80):
-    dayCycle(swarm)
+    for day in range(days):
+        reproductiveFishCount = swarmAgeDistribution[0]
+        for ageCounter in range(1,9):
+            swarmAgeDistribution[ageCounter - 1] = swarmAgeDistribution[ageCounter]
+        swarmAgeDistribution[8] = reproductiveFishCount
+        swarmAgeDistribution[6] += reproductiveFishCount
 
-print("2021 Day 6, Part 1: " + str(len(swarm)))
+    return sum(swarmAgeDistribution)
 
-swarmAgeDistribution = []
-for i in range(9):
-    swarmAgeDistribution.append(0)
-
-for fish in input[0].split(','):
-    swarmAgeDistribution[int(fish)] += 1
-
-for day in range(256):
-    reproductiveFishCount = swarmAgeDistribution[0]
-
-    for ageCounter in range(1,9):
-        swarmAgeDistribution[ageCounter - 1] = swarmAgeDistribution[ageCounter]
-
-    swarmAgeDistribution[8] = reproductiveFishCount
-    swarmAgeDistribution[6] += reproductiveFishCount
-
-sum = 0
-for fishCount in swarmAgeDistribution:
-    sum += fishCount
-
-print("2021 Day 6, Part 2: " + str(sum))
+print("2021 Day 6, Part 1: " + str(getSwarmAfterDays(80)))
+print("2021 Day 6, Part 2: " + str(getSwarmAfterDays(256)))
