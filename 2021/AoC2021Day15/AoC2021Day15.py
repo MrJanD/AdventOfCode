66d234b7c075519863451bc79bad39e572dfe69e
JanD <jan.deppe@gmx.com>
2021-12-16 00:21:10 +0100

    Day 15 complete

--- 2021/AoC2021Day15/AoC2021Day15.py
+++ 2021/AoC2021Day15/AoC2021Day15.py
@@ -2,13 +2,13 @@ import sys
 import numpy as np
 
 class Point():
-    def __init__(self, p, value):
+    def __init__(self, p, value, size):
         self.Coords = p
         self.Value = value
         self.Visited = False
-        self.ShortestDistance = 50 * 50 * 9 #
+        self.ShortestDistance = 500 * 500 * 9 #
         self.PreviousPoint = (0, 0)
-        self.MatrixSize = (10, 10) #
+        self.MatrixSize = size
 
     def refreshPoint(self, preDst, preP):
         if (preDst + self.Value) < self.ShortestDistance:
@@ -17,11 +17,11 @@ class Point():
 
     def getAdjacents(self):
         adj = []
-        if self.MatrixSize[0] > self.Coords[0] + 1:
+        if self.MatrixSize > self.Coords[0] + 1:
             adj.append((self.Coords[0] + 1, self.Coords[1]))
         if self.Coords[0] - 1 >= 0:
             adj.append((self.Coords[0] - 1, self.Coords[1]))
-        if self.MatrixSize[1] > self.Coords[1] + 1:
+        if self.MatrixSize > self.Coords[1] + 1:
             adj.append((self.Coords[0], self.Coords[1] + 1))
         if self.Coords[1] - 1 >= 0:
             adj.append((self.Coords[0], self.Coords[1] - 1))
@@ -32,82 +32,45 @@ class Point():
             mtrx[adj[0]][adj[1]].refreshPoint(self.ShortestDistance, self.Coords)
         self.Visited = True
 
-    def print(self):
-        print("Coords: " + str(self.Coords[0]) + ", " + str(self.Coords[1]))
-        print("Value: " + str(self.Value))
-        print("ShortestDistance: " + str(self.ShortestDistance))
-        print("PreviousPoint: " + str(self.PreviousPoint[0]) + ", " + str(self.PreviousPoint[1]))
-        print("MatrixSize: " + str(self.MatrixSize[0]) + ", " + str(self.MatrixSize[1]))
-
 #Parse input
-input = [line.strip() for line in open("../Inputs/day15.ex", "r")]
+input = [line.strip() for line in open("../Inputs/day15", "r")]
 matrix = []
 for idy, line in enumerate(input):
-    matrix.append([Point((idy, idx), int(n)) for idx, n in enumerate(line)])
+    matrix.append([Point((idy, idx), int(n), 100) for idx, n in enumerate(line)])
 
-matrixList = []
-for line in input:
-    matrixList.append([int(n) for n in line])
-
+# Dijkstra without remembering which node already visited
 def dijkstra(depth, mat):
     for d in range(depth + 1):
         mat[d][depth].updateAdjacents(mat)
         mat[depth][d].updateAdjacents(mat)
         mat[d][d].updateAdjacents(mat)
 
-matrixSmall2 = matrix.copy()
-
 matrix[0][0].ShortestDistance = 0
-for i in range(10): #
+for i in range(100): #
     dijkstra(i, matrix)
-print("2021 Day 15, Part 1: " + str(matrix[9][9].ShortestDistance)) #
+print("2021 Day 15, Part 1: " + str(matrix[99][99].ShortestDistance)) #
 
-#matrixList2 = []
-#for x in range(len(matrixList2) * 5):
-#    addX = x//100
-#    xLine = []
-#    for y in range(len(matrixList2) * 5):
-#        addY = y//100
-#        value = matrixList2[x % 100][y % 100] + addX + addY
-#        if value > 9:
-#            value -= 9
-#        print("WRONG VALUE IN: " + str(x) + ", " + str(y))
-#        xLine.append(value)
-#    matrixList2.append(xLine)
-
-#resultMatrix2 = [[(500 * 500 * 10) for _ in range(500)] for _ in range(500)]
-
-#l = [[0, 0], [0, 0]]
-#m = l.copy()
+resultMatrix2 = [[(500 * 500 * 10) for _ in range(500)] for _ in range(500)]
 
 matrix2 = []
-for x in range(len(matrixSmall2) * 5):
-    addX = x//10 #
+for x in range(len(matrix) * 5):
+    addX = x//100 #
     xLine = []
-    for y in range(len(matrixSmall2) * 5):
-        addY = y//10 #
-        value = matrixSmall2[x % 10][y % 10].Value + addX + addY #
-        p = Point((x, y), value)
-        p.MatrixSize = (50, 50) #
-        xLine.append(p)
+    for y in range(len(matrix) * 5):
+        addY = y//100 #
+        value = matrix[x % 100][y % 100].Value + addX + addY #
+        if value > 9:
+            value -= 9
+        xLine.append(Point((x, y), value, 500))
     matrix2.append(xLine)
 
-for x in range(len(matrix2)):
-    for y in range(len(matrix2[0])):
-        if matrix2[x][y].Value > 9:
-            matrix2[x][y].Value -= 9
-        if matrix2[x][y].Value < 1 or matrix2[x][y].Value > 9:
-            print("WRONG VALUE IN: ")
-            matrix2[x][y].print()
-
 matrix2[0][0].ShortestDistance = 0
-for i in range(50): #
-    dijkstra(i, matrix2)
+shortestDst = 500 * 500 * 9
+while True:
+    for i in range(500): #
+        dijkstra(i, matrix2)
+    if shortestDst == matrix2[499][499].ShortestDistance:
+        break
+    shortestDst = matrix2[499][499].ShortestDistance
 
-
-print("2021 Day 15, Part 2: ")
-#matrix2[499][499].print()
-#print("498, 499: ")
-#matrix2[498][499].print()
-#print("499, 498: ")
-#matrix2[499][498].print()
+print("2021 Day 15, Part 2: " + str(shortestDst))
